3e2f6b3351a00f1f9ccee54e563f104aa67975af
Sinameru <sinameru@localhost.localdomain>
2025-10-16 21:40:38 +0100

    test

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -4,6 +4,6 @@ output "s3_bucket_name" {
 }
 
 output "s3_bucket_arn" {
-  description = "The ARN of the S3 bucket"
+  description = "The ARN of the S3 bucket ;)"
   value       = aws_s3_bucket.s3_gdp.arn
 }
